336a85175a5f6d3f7e5bad304e161973cabe4793
rr33dd11 <99999dimka99999@gmail.com>
2024-09-22 15:08:42 +0700

    adding authConfig for OAuth2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './oauth/authConfig.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthProvider authConfig={authConfig}>
+      <App />
+  </AuthProvider>
 )
